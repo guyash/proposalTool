e569ecd9aefc4450f9092ef1e81f7e3e6781e899
Guy-VisionVoyage <guy@visionvoyage.ai>
2025-01-30 22:32:04 -0100

    bug fixes

---
+++ src/aws-config.js
@@ -0,0 +1,7 @@
+const awsConfig = {
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  region: process.env.REACT_APP_AWS_REGION,
+};
+
+export default awsConfig;
