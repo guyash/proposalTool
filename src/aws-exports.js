170bf1da5d086bd6ae218fe7cb038d2219ae0398
Guy-VisionVoyage <guy@visionvoyage.ai>
2025-01-31 17:18:17 -0100

    test 7

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,33 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://vovy5y4xzjctppsgygtowvlwt4.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-2ceitradknbaplbqeez745dtle",
-    "aws_cognito_identity_pool_id": "us-east-1:3a69e240-df3d-4760-bf4d-c99995cc6deb",
-    "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_l2fpNYaVf",
-    "aws_user_pools_web_client_id": "4gs7gf9f2o93so9dnrf4tpkefe",
-    "oauth": {},
-    "aws_cognito_username_attributes": [
-        "EMAIL"
-    ],
-    "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [],
-    "aws_cognito_mfa_configuration": "OFF",
-    "aws_cognito_mfa_types": [
-        "SMS"
-    ],
-    "aws_cognito_password_protection_settings": {
-        "passwordPolicyMinLength": 8,
-        "passwordPolicyCharacters": [
-            "REQUIRES_LOWERCASE",
-            "REQUIRES_NUMBERS",
-            "REQUIRES_SYMBOLS",
-            "REQUIRES_UPPERCASE"
-        ]
-    },
-    "aws_cognito_verification_mechanisms": [
-        "EMAIL"
-    ]
+    "aws_appsync_apiKey": "da2-2ceitradknbaplbqeez745dtle"
 };
 
 
